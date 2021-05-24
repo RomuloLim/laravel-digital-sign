0cc0a6ac196ca79d4d65e2005914be5804f460a0
Rômulo Lima <37809622+RomuloLim@users.noreply.github.com>
2021-05-24 11:30:43 -0300

    Finalizado

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -75,37 +75,37 @@ class PdfController extends Controller
 
         // pegando o conteúdo do certificado .pfx
         $data = file_get_contents($certificate);
+        // verificando se o conteúdo do certificado .pfx é válido
+        if (openssl_pkcs12_read($data, $certs, $request->password)) {
+            // coloca os dados do certificado em $certs['cert'] e a chave privada em $certs['pkey']
+            file_put_contents($certificate, $certs['cert'] . $certs['pkey']);
 
-                // verificando se o conteúdo do certificado .pfx é válido
-                if (openssl_pkcs12_read($data, $certs, $request->password)) {
-                // coloca os dados do certificado em $certs['cert'] e a chave privada em $certs['pkey']
-                file_put_contents($certificate, $certs['cert'] . $certs['pkey']);
+            // converte o certificado e pega os dados do proprietário
+            $content = openssl_x509_parse(openssl_x509_read($certs['cert']));
 
-                // converte o certificado e pega os dados do proprietário
-                $content = openssl_x509_parse(openssl_x509_read($certs['cert']));
+            // dd($content);
+            // variável da razão social e CNPJ/CPF
+            $info = explode(':', $content['subject']['CN']);
+            $hashInfo = md5($info[1]);
+            // salva o nome da empresa
+            $company = $info[0];
 
-                // variável da razão social e CNPJ/CPF
-                $info = explode(':', $content['subject']['CN']);
-                $hashInfo = md5($info[1]);
-                // salva o nome da empresa
-                $company = $info[0];
-
-                // verifica se o registro é um CNPJ ou CPF e salva o registro
-                if (strlen($info[1]) == 14) {
-                    $type = 'CNPJ: ' . $info[1];
-                } else {
-                    $type = 'CPF: ' . $info[1];
-                }
-
-                // informação para a assinatura digital, contendo o nome da empresa, nº do registro, data e hora da assinatura
-                $info = $company . "\n" . $type . "\n" . 'Data: ' . date('d/m/Y H:i');
+            // verifica se o registro é um CNPJ ou CPF e salva o registro
+            if (strlen($info[1]) == 14) {
+                $type = 'CNPJ: ' . $info[1];
             } else {
-                    // removendo o arquivo .pfx do local público
-                    File::delete($certificate);
+                $type = 'CPF: ' . $info[1];
+            }
 
-                    // se o conteúdo do arquivo .pfx for inválido
-                    return redirect()->back()->with('status', 'Arquivo .pfx ou senha inválida.');
-                }
+            // informação para a assinatura digital, contendo o nome da empresa, nº do registro, data e hora da assinatura
+            $info = $company . "\n" . $type . "\n" . 'Data: ' . date('d/m/Y H:i');
+        } else {
+            // removendo o arquivo .pfx do local público
+            File::delete($certificate);
+
+            // se o conteúdo do arquivo .pfx for inválido
+            return redirect()->back()->with('status', 'Arquivo .pfx ou senha inválida.');
+        }
 
 
         /*
@@ -122,7 +122,7 @@ class PdfController extends Controller
         $onlyName = explode('/', $pdfToSign[0]->file_name);
         $onlyName = $onlyName[1];
 
-        $pdf = new Fpdi();
+        $pdf = new Fpdi('P', 'mm', 'P', true, 'UTF-8', false);
         $pdf->SetTitle($onlyName);
         // assinando o PDF com a certificado (assinatura digital)
         $pdf->setSignature($certificate, $certificate, '', '', 2, '', 'A');
@@ -141,7 +141,11 @@ class PdfController extends Controller
             $pdf->Cell(0, 0, "Chave de acesso ${hashInfo}", 1, 1, 'C', 0, '', 0);
         }
 
-        $filenamePDF = md5($file->getClientOriginalName() . '-' . implode('-', explode(':', date('H:i:s'))).'.pdf');
+        $pdf->setSignatureAppearance(180, 60, 15, 15);
+
+        // dd($pdf);
+
+        // $filenamePDF = md5($file->getClientOriginalName() . '-' . implode('-', explode(':', date('H:i:s'))).'.pdf');
         $pdf->Output($path.'/'.$onlyName, 'FI'); // salva em um diretório e mostra na tela
         // removendo o arquivo .pfx do local público
         File::delete($certificate);
